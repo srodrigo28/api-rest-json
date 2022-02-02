008b0f2ccbb5dba69321fe8694ed21a5adf4188d
srodrigo28 <rodrigoexer1@gmail.com>
2022-02-02 07:39:11 -0300

    atualizado o frontend

--- frontend/user2/script.js
+++ frontend/user2/script.js
@@ -1,2 +1,57 @@
-alert('hello, Javascript')
+//alert('hello, Javascript')
 //  http://localhost:3001/users
+
+let tr = ''
+let id = 1
+let OneUser = ''
+let url = 'http://localhost:3001/users'
+
+async function getUsers(){
+    try{
+        const response = await fetch(url)
+        const users = await response.json()
+        //console.log(data)
+        // output
+        if(users){
+            
+            for(let user of users){
+                tr += `
+                    <tr>
+                        <td>${user.id} </td>
+                        <td>${user.name} </td>
+                        <td>${user.email} </td>
+                        <td>${user.city} </td>
+                    </tr>
+                `
+            }
+            document.querySelector('#tbody').innerHTML = tr
+        }else{
+            console.log(users)
+        }
+    }catch(error){
+        console.log(error)
+    }
+}
+getUsers()
+function getOneUser(){
+    axios.get(`${url}/${id}`)
+    .then(response => {
+        const data = response.data
+        OneUser += `
+            <p>${data.name}</p>
+        `
+        //renderResults.textContent = JSON.stringify(data)
+        document.querySelector('main').innerHTML = OneUser
+    }).catch(error => console.log(error))
+}
+getOneUser()
+
+// function getUsers(data){
+//     console.log(data)
+//     // let output = ''
+//     // for(let user of data){
+//     //     output += `<li>${user.city}</li>`
+//     // }
+//     // document.querySelector('main').innerHTML = output
+// }
+// getUsers()
